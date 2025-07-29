66bffb381ce3b45f1c3fbd9ae7b14084002c0aae
Chris Weinberger <cleeweinberger@gmail.com>
2025-07-29 21:45:44 +0000

    square file

---
+++ src/geometry/square.py
@@ -0,0 +1,15 @@
+def calculate_area(length):
+    """
+    Calculate area of a square.
+
+    Parameters
+    ----------
+    length : float
+        The length of a square.
+
+    Returns
+    -------
+    area : float
+        The area calculated.
+    """
+    return length ** 2
